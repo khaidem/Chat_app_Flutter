257db4ce9150a69d7fd076eda80203e28f109553
khaidem <khaidemdenish@gmail.com>
2022-10-02 21:40:32 +0530

    ui change in phone

--- lib/src/Home/widgets/phone_verification.widget.dart
+++ lib/src/Home/widgets/phone_verification.widget.dart
@@ -39,7 +39,9 @@ class _PhoneNumberVerificationWidgetState
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         leading: IconButton(
@@ -53,169 +55,170 @@ class _PhoneNumberVerificationWidgetState
         ),
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(children: [
-            Container(
-              width: 200,
-              margin: const EdgeInsets.all(10),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
                   'assets/images/phone_verifciation_image.png',
+                  height: size.height * 0.3,
                 ),
-              ),
-            ),
-            Container(
-              // width: 400,
-              margin: const EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Enter your Phone Number",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text(
-                    ' we will send you one Time Code on your phone number',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    height: 55,
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 1, color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        SizedBox(
-                          width: 40,
-                          child: TextFormField(
-                            controller: countryController,
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              border: InputBorder.none,
-                            ),
-                          ),
-                        ),
-                        const Text(
-                          "|",
-                          style: TextStyle(
-                            fontSize: 33,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        SizedBox(
-                          width: 300,
-                          child: TextFormField(
-                            // maxLength: 10,
-                            controller: phoneController,
-                            onChanged: (value) {
-                              if (value.length == 10) {
-                                setState(
-                                  () {
-                                    enableButton = true;
-                                    phone = value;
-                                  },
-                                );
-                              } else {
-                                enableButton = false;
-                              }
-                            },
-                            keyboardType: TextInputType.phone,
-                            decoration: const InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Enter Phone number",
-                            ),
-                          ),
-                        )
-                      ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Enter your Phone Number",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 45,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.green.shade600,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: const [
+                    Text(
+                      ' we will send you one Time Code on your phone number',
+                      style: TextStyle(fontSize: 15, color: Colors.black45),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 55,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 40,
+                        child: TextFormField(
+                          controller: countryController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                          ),
                         ),
                       ),
-                      onPressed: enableButton
-                          ? () async {
-                              if (mounted) {
-                                setState(() {
-                                  _isLoading = true;
-                                });
-                              }
-
-                              try {
-                                await context
-                                    .read<AuthProvider>()
-                                    .verificationPhone(
-                                      context,
-                                      countryController.text + phone,
-                                    );
-                                log(phone.toString());
-
-                                await Future.delayed(
-                                  const Duration(seconds: 5),
-                                );
-                              } on FirebaseAuthException catch (error) {
-                                debugPrint(
-                                  error.toString(),
-                                );
-                              }
-                              if (mounted) {
-                                setState(
-                                  () {
-                                    _isLoading = false;
-                                  },
-                                );
-                              }
+                      const Text(
+                        "|",
+                        style: TextStyle(
+                          fontSize: 33,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 300,
+                        child: TextFormField(
+                          // maxLength: 10,
+                          controller: phoneController,
+                          onChanged: (value) {
+                            if (value.length == 10) {
+                              setState(
+                                () {
+                                  enableButton = true;
+                                  phone = value;
+                                },
+                              );
+                            } else {
+                              enableButton = false;
                             }
-                          : null,
-                      child: _isLoading
-                          ? const SizedBox(
-                              width: 16,
-                              height: 16,
-                              child: CircularProgressIndicator(
-                                color: Colors.white,
-                                strokeWidth: 1.5,
-                              ),
-                            )
-                          : const Text("Send the code"),
-                    ),
+                          },
+                          keyboardType: TextInputType.phone,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Enter Phone number",
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  TextButton(
-                    onPressed: () {
-                      phoneController.clear();
-                    },
-                    child: const Text('Cancel'),
-                  )
-                ],
-              ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green.shade600,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: enableButton
+                        ? () async {
+                            if (mounted) {
+                              setState(() {
+                                _isLoading = true;
+                              });
+                            }
+
+                            try {
+                              await context
+                                  .read<AuthProvider>()
+                                  .verificationPhone(
+                                    context,
+                                    countryController.text + phone,
+                                  );
+                              log(phone.toString());
+
+                              await Future.delayed(
+                                const Duration(seconds: 5),
+                              );
+                            } on FirebaseAuthException catch (error) {
+                              debugPrint(
+                                error.toString(),
+                              );
+                            }
+                            if (mounted) {
+                              setState(
+                                () {
+                                  _isLoading = false;
+                                },
+                              );
+                            }
+                          }
+                        : null,
+                    child: _isLoading
+                        ? const SizedBox(
+                            width: 16,
+                            height: 16,
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                              strokeWidth: 1.5,
+                            ),
+                          )
+                        : const Text("Send the code"),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    phoneController.clear();
+                  },
+                  child: const Text('Cancel'),
+                ),
+                // const Spacer(
+                //   flex: 1,
+                // )
+              ],
             ),
-          ]),
+          ),
         ),
       ),
     );
